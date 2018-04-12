8638f2f96a09db20f98460d698ca52cd35385fdc
Namibia Torres <namibialt@gmail.com>
2018-04-12 15:41:09 +1000

    add state and 3 type annotations

--- src/index.js
+++ src/index.js
@@ -3,12 +3,37 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import foxesData from "./foxes.json";
 
-class App extends Component {
+type Post = {
+  id: string,
+  userId: string,
+  description: string,
+  imageUrl: string
+};
+
+type User = {
+  id: string,
+  username: string
+};
+
+type Props = {
+  data: {
+    posts: Array<Post>,
+    users: Array<User>
+  }
+}
+
+class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+  //     value: null
+  //   }
+  // }
   render() {
-    return <div>Hello World!</div>;
-    console.log(this.props.data)
+    console.log(this.state.value);
+    return <div>Hello World!</div>
   }
 }
 
 const appRoot = document.getElementById("root");
-appRoot && ReactDOM.render(<App foxesData="data" />, appRoot);
+appRoot && ReactDOM.render(<App data={foxesData} />, appRoot);
