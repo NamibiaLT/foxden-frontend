9aaeac15526d84ea14f3f5ca0c7e59bd97fae92b
Namibia Torres <namibialt@gmail.com>
2018-04-13 15:32:20 +1000

    update tests with enzyme imports

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,36 @@
-const {
-  renderPost
-} = require("./index.js");
+import React from 'react';
+import { mount } from 'enzyme';
+import App from './index.js';
 
-
-describe("renderPost", () => {
-  test("renders pictures of foxes", () => {
-    expect(renderPost(post)).toEqual([<img src={post.imageUrl}/>]);
-  });
+it('renders without crashing', () => {
+    const post = {
+      id: '123',
+      description: 'cute fox',
+      imageUrl: 'link.com'
+    }
+    const data = {
+      posts: [post]
+    }
+  expect(mount(<App data={data}/>).html()).toBe(<div><img src="link.com" key="123" alt="cute fox" /></div>);
+  // expect(shallow()).to.have.property
 });
+
+// import React from 'react';
+// import {shallow} from 'enzyme';
+// import App from "./index.js";
+
+// describe("App", () => {
+//   test("renders pictures of foxes", () => {
+//     const post = {
+//       id: '123',
+//       description: 'cute fox',
+//       imageUrl: 'link.com'
+//     }
+//     const data = {
+//       posts: [post]
+//     }
+//     const app = shallow(<App data={data} />)
+//     console.log(app.html())
+//     expect(app.html()).toEqual('<img src="link.com"/>')
+//   });
+// });
